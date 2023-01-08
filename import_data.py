a12b20e5420a623b40750a10c1328a32eb759e35
AlexandrKletke <klete@mail.ru>
2023-01-08 20:31:19 +0300

    change module import data

--- import_data.py
+++ import_data.py
@@ -1,29 +1,36 @@
 # модуль Добавление данных
  
-"""def import_data(data, separator = None):
+"""def import_data(data, table = None):
     with open('phone_book.csv', 'a+') as file:
-        if separator == None:
+        if table == None:
             for i in data:
                 file.write(f"{i}\n")
             file.write(f"\n")
         else:
-            file.write(separator.join(data))
+            file.write(table.join(data))
             file.write(f"\n")"""
 
-def import_data(data, separator):
-    if separator == None:
-        with open('phone_book1.csv', 'a+') as file1:
-            file1.write(f"{data[0]}\n")
-            file1.write(f"{data[1]}\n")
-            file1.write(f"{data[2]}\n")
-            file1.write(f"{data[3]}\n")
+def import_data(data, table):
+    for id in enumerate(data, start=1):
+        if table == 1:
+            with open('student_database.csv', 'a+') as f1:
+                f1.write(id.join(data))
+                f1.write(f"\n")
+            """f1.write(f"{data[0]}\n")
+            f1.write(f"{data[1]}\n")
+            f1.write(f"{data[2]}\n")
+            f1.write(f"{data[3]}\n")"""
             """for i in data:
-                file1.write(f"{i[0]}\n")
-                file1.write(f"{i[1]}\n")
-                file1.write(f"{i[2]}\n")
-                file1.write(f"{i[3]}\n")"""
-            file1.write(f"\n")
-    else:
-        with open('phone_book2.csv', 'a+') as file2:
-            file2.write(separator.join(data))
-            file2.write(f"\n")
+                f1.write(f"{i[0]}\n")
+                f1.write(f"{i[1]}\n")
+                f1.write(f"{i[2]}\n")
+                f1.write(f"{i[3]}\n")
+            f1.write(f"\n")"""
+        elif table == 2:
+            with open('lesson_database.csv', 'a+') as f2:
+                f2.write(id.join(data))
+                f2.write(f"\n")
+        else:
+            with open('symbolclass_database.csv', 'a+') as f3:
+                f3.write(id.join(data))
+                f3.write(f"\n")
