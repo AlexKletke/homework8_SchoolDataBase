eec160bccb8f9f0335228abd030ed61a57fd9251
AlexandrKletke <klete@mail.ru>
2023-01-13 00:01:50 +0300

    change module del

--- delete_data.py
+++ delete_data.py
@@ -2,20 +2,20 @@
 
 def delete_data(table, id_line):
     if table == 1:
-        lines = open('student_database.csv', 'r').readlines()
-        lines.remove(id_line-1)
-        out = open('student_database.csv', 'w')
+        lines = open('student_database.txt', 'r').readlines()
+        lines[id_line-1] = ''
+        out = open('student_database.txt', 'w')
         out.writelines(lines)
         out.close()
     elif table == 2:
-        lines = open('lesson_database.csv', 'r').readlines()
-        lines.remove(id_line-1)
-        out = open('lesson_database.csv', 'w')
+        lines = open('lesson_database.txt', 'r').readlines()
+        lines[id_line-1] = ''
+        out = open('lesson_database.txt', 'w')
         out.writelines(lines)
         out.close()
     else:
-        lines = open('symbolclass_database.csv', 'r').readlines()
-        lines.remove(id_line-1)
-        out = open('symbolclass_database.csv', 'w')
+        lines = open('symbolclass_database.txt', 'r').readlines()
+        lines[id_line-1] = ''
+        out = open('symbolclass_database.txt', 'w')
         out.writelines(lines)
         out.close()
