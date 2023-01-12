40a264fd1fd57bddab151b9e9714cd60a9dfc0bb
AlexandrKletke <klete@mail.ru>
2023-01-13 00:44:34 +0300

    change exp module

--- export_data.py
+++ export_data.py
@@ -2,47 +2,24 @@
 
 def export_data(table):
     if table == 1:
-        with open('student_database.csv', 'r') as file1:
+        with open('student_database.txt', 'r') as f1:
             data1 = []
-            for line in file1:
+            for line in f1:
                 if line != '\n':
                     data1.append(line.strip())
-                data = data1
+        data = data1
     if table == 2:
-        with open('lesson_database.csv', 'r') as file2:
+        with open('lesson_database.txt', 'r') as file2:
             data2 = []
             for line in file2:
                 if line != '\n':
                     data2.append(line.strip())
-                data = data2
+        data = data2
     else:
-        with open('symbolclass_database.csv', 'r') as file3:
+        with open('symbolclass_database.txt', 'r') as file3:
             data3 = []
             for line in file3:
                 if line != '\n':
                     data3.append(line.strip())
-                data = data3
-        # t = []
-            """for line in file2:
-                if ',' in line:
-                # temp = line.strip().split(',')
-                    temp = line.split(',')
-                    data2.append(temp)
-                elif ';' in line:
-                    temp = line.split(';')
-                    data2.append(temp)
-                elif '-' in line:
-                    temp = line.split('-')
-                    data2.append(temp)
-                elif '*' in line:
-                    temp = line.split('*')
-                    data2.append(temp)
-                data = data2 """
-
-            """elif line != '':
-                if line != '\n':
-                    t.append(line.strip())
-                else:
-                    data.append(t)
-                    t= []"""
+        data = data3       
     return data
