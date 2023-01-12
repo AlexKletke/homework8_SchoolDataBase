eb2997a2d898e7a2a1192ec516e7ee149f5e4d2f
AlexandrKletke <klete@mail.ru>
2023-01-13 00:15:10 +0300

    change red module

--- redact_data.py
+++ redact_data.py
@@ -2,23 +2,26 @@
 
 def redact_data(table, id_line):
     if table == 1:
-        lines = open('student_database.csv', 'r').readlines()
+        lines = open('student_database.txt', 'r').readlines()
         print(f"данные для корректировки: {lines[id_line-1]}")
         lines[id_line-1] = input('введите новые данные, соблюдая те же знаки препинания: ')
-        out = open('student_database.csv', 'w')
+        out = open('student_database.txt', 'w')
         out.writelines(lines)
+        out.write(f"\n")
         out.close()
     elif table == 2:
-        lines = open('lesson_database.csv', 'r').readlines()
+        lines = open('lesson_database.txt', 'r').readlines()
         print(f"данные для корректировки: {lines[id_line-1]}")
         lines[id_line-1] = input('введите новые данные, соблюдая те же знаки препинания: ')
-        out = open('lesson_database.csv', 'w')
+        out = open('lesson_database.txt', 'w')
         out.writelines(lines)
+        out.write(f"\n")
         out.close()
     else:
-        lines = open('symbolclass_database.csv', 'r').readlines()
+        lines = open('symbolclass_database.txt', 'r').readlines()
         print(f"данные для корректировки: {lines[id_line-1]}")
         lines[id_line-1] = input('введите новые данные, соблюдая те же знаки препинания: ')
-        out = open('symbolclass_database.csv', 'w')
+        out = open('symbolclass_database.txt', 'w')
         out.writelines(lines)
+        out.write(f"\n")
         out.close()
