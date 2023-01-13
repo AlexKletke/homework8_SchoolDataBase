1bb0720acc277836f26bf2e787fd4583cbf0a4c4
AlexandrKletke <klete@mail.ru>
2023-01-13 16:30:19 +0300

    change mod import

--- import_data.py
+++ import_data.py
@@ -2,21 +2,20 @@
  
 def import_data(data, table):
     if table == 1:
-        with open('student_database.txt', 'r') as read_object:
-            lines = read_object.readlines()
+        #with open('student_database.txt', 'r') as read_object:
+           # lines = read_object.readlines()
         with open('student_database.txt', 'a') as f1:
-            f1.write(f'{len(lines)+1}, {data}')
+            #f1.write(f'{len(lines)+1}, {data}')
+            f1.writelines(f'{data}')
             f1.write(f"\n")            
     elif table == 2:
-        with open('lesson_database.txt', 'r') as read_object:
-            lines = read_object.readlines()            
         with open('lesson_database.txt', 'a') as f2:            
-            f2.write(f'{len(lines)+1}, {data}')
+            f2.writelines(f'{data}')
             f2.write(f"\n")       
-    else:
-        with open('symbolclass_database.txt', 'r') as read_object:
-            lines = read_object.readlines()
+    else:       
         with open('symbolclass_database.txt', 'a') as f3:
-            f3.write(f'{len(lines)+1}, {data}')
+            f3.writelines(f'{data}')
             f3.write(f"\n")
+
+
             
